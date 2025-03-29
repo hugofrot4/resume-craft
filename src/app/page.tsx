3ab20021097f6b28b1f6b2adb1e21d163219833a
Hugo Frota <herbtyh@gmail.com>
2025-03-29 19:50:36 -0300

    feat:dashboard layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="font-title">
-      <h1>Wellcome to Resume Craft</h1>
+      <h1>Wellcome to ResumeCraft</h1>
     </main>
   );
 }
